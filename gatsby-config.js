2965be75c8532660c4cbcd0be7ade6f3234d0c08
Pankaj Pawar <pankaj@Pankajs-MacBook-Air.local>
2024-01-23 00:44:05 +0530

    Manifest Name Change

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ChandrikaDeb',
-        short_name: 'ChandrikaDeb',
+        name: config.name,
+        short_name: config.name,
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
